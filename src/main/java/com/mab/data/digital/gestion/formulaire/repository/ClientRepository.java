03cc1b0490c24cdd6930afe63b297830f801a37b
ali.aich <ali.aich@sis-france.com>
2021-05-29 16:58:33 +0200

    la correction de la requête native

--- src/main/java/com/mab/data/digital/gestion/formulaire/repository/ClientRepository.java
+++ src/main/java/com/mab/data/digital/gestion/formulaire/repository/ClientRepository.java
@@ -10,7 +10,7 @@ import com.mab.data.digital.gestion.formulaire.domain.Client;
 @Repository
 public interface ClientRepository extends CrudRepository<Client, Integer> {
 
-    @Query("SELECT c FROM Client c WHERE c.nom= :nom and c.prenom= :prenom and c.adresse= :adresse and c.cp= :cp and c.adresseMail=:adressemail and c.tel=:tel")
+    @Query(value = "SELECT * FROM Client c WHERE c.nom= :nom and c.prenom= :prenom and c.adresse= :adresse and c.cp= :cp and c.adresse_mail=:adressemail and c.tel=:tel", nativeQuery = true)
     public Client siClientExiste(@Param("nom") String nom, @Param("prenom") String prenom, @Param("adresse") String adresse, @Param("cp") String cp,
 	    @Param("adressemail") String adressemail, @Param("tel") String tel);
 }
